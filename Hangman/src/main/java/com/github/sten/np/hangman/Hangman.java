ae21d938d885bbbb5b4ab94974febe2a945d83ea
Sten Nierop <steennierop@gmail.com>
2024-04-10 11:34:24 +0200

    Project Setup.

---
+++ Hangman/src/main/java/com/github/sten/np/hangman/Hangman.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.github.sten.np.hangman;
+
+/**
+ *
+ * @author steen
+ */
+public class Hangman {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
